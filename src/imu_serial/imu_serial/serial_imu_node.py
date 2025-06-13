2bf4d01e5f2f86ea029ceae57d39258b0595d30b
ichsanyudika <ichsanfajar1204@gmail.com>
2025-06-13 10:52:46 +0700

    Update

--- src/imu_serial/imu_serial/serial_imu_node.py
+++ src/imu_serial/imu_serial/serial_imu_node.py
@@ -7,7 +7,7 @@ from geometry_msgs.msg import Quaternion
 import math
 
 def euler_to_quaternion(roll, pitch, yaw):
-    # Konversi dari Euler (rad) ke Quaternion
+    # Conversion from Euler (rad) to Quaternion
     cy = math.cos(yaw * 0.5)
     sy = math.sin(yaw * 0.5)
     cp = math.cos(pitch * 0.5)
@@ -28,7 +28,6 @@ class IMUNode(Node):
         self.publisher_ = self.create_publisher(Imu, 'imu/data_raw', 10)
         self.timer = self.create_timer(0.01, self.timer_callback)
 
-        # Ganti sesuai port IMU Razor kamu
         self.serial_port = serial.Serial('/dev/ttyACM0', 115200, timeout=1)
 
     def timer_callback(self):
@@ -38,25 +37,25 @@ class IMUNode(Node):
 
             imu_msg = Imu()
 
-            # Header untuk sinkronisasi RViz
+            # RViz
             imu_msg.header.stamp = self.get_clock().now().to_msg()
             imu_msg.header.frame_id = 'base_link'
 
-            # Ambil data Euler (dalam derajat), konversi ke radian
+            # Euler data (in degrees), convert to radians
             roll = math.radians(data['roll'])
             pitch = math.radians(data['pitch'])
             yaw = math.radians(data['yaw'])
 
-            # Konversi ke Quaternion
+            # Convert to Quaternion
             quat = euler_to_quaternion(roll, pitch, yaw)
             imu_msg.orientation = quat
 
-            # Orientation covariance (diisi float, harus 9 elemen)
+            # Orientation covariance
             imu_msg.orientation_covariance = [0.02, 0.0, 0.0,
                                               0.0, 0.02, 0.0,
                                               0.0, 0.0, 0.02]
 
-            # Akselerasi (dalam m/s²)
+            # Acceleration (in m/s²)
             imu_msg.linear_acceleration.x = float(data['accX'])
             imu_msg.linear_acceleration.y = float(data['accY'])
             imu_msg.linear_acceleration.z = float(data['accZ'])
@@ -64,7 +63,7 @@ class IMUNode(Node):
                                                       0.0, 0.1, 0.0,
                                                       0.0, 0.0, 0.1]
 
-            # Angular velocity kosong (jika IMU Razor tidak mengirim)
+            # Angular velocity is empty (if the Razor IMU is not sending)
             imu_msg.angular_velocity.x = 0.0
             imu_msg.angular_velocity.y = 0.0
             imu_msg.angular_velocity.z = 0.0
@@ -72,7 +71,7 @@ class IMUNode(Node):
                                                    0.0, 0.05, 0.0,
                                                    0.0, 0.0, 0.05]
 
-            # Publikasi ke ROS
+            # Publishto ROS
             self.publisher_.publish(imu_msg)
 
         except (json.JSONDecodeError, KeyError, ValueError):
